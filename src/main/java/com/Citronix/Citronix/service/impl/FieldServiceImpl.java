53a306c12d583be110ee50666babe7307b699236
Khadija2504 <khadijalady25@gmail.com>
2024-11-20 09:15:20 +0100

    CIT-14 check if the total area in the farm equals the total fields area to handl the excepssions

--- src/main/java/com/Citronix/Citronix/service/impl/FieldServiceImpl.java
+++ src/main/java/com/Citronix/Citronix/service/impl/FieldServiceImpl.java
@@ -1,5 +1,6 @@
 package com.Citronix.Citronix.service.impl;
 
+import com.Citronix.Citronix.model.Farm;
 import com.Citronix.Citronix.model.Field;
 import com.Citronix.Citronix.repository.FieldRepository;
 import com.Citronix.Citronix.service.FieldService;
@@ -15,8 +16,26 @@ public class FieldServiceImpl implements FieldService {
 
     @Override
     public Field addField(Field field) {
+        Farm farm = field.getFarm();
+
+        if (farm == null) {
+            throw new IllegalArgumentException("Field must be associated with a valid farm.");
+        }
+
+        List<Field> fields = fieldRepository.findByFarm(farm);
+        double totalFieldsArea = 0.0;
+        if (!fields.isEmpty()) {
+            totalFieldsArea = fields.stream()
+                    .mapToDouble(Field::getArea)
+                    .sum();
+        }
+
+        if (totalFieldsArea + field.getArea() > farm.getArea()) {
+            throw new IllegalArgumentException("The total area of fields exceeds the farm's area.");
+        }
         return fieldRepository.save(field);
     }
+
 
     @Override
     public List<Field> getFields() {
