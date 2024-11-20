1197e68b4efb94053b3cacdd55c528bd1f9836df
Khadija2504 <khadijalady25@gmail.com>
2024-11-20 22:09:31 +0100

    CIT-18 display, update and delete tree with calcs its age

--- src/main/java/com/Citronix/Citronix/service/impl/TreeServiceImpl.java
+++ src/main/java/com/Citronix/Citronix/service/impl/TreeServiceImpl.java
@@ -6,6 +6,8 @@ import com.Citronix.Citronix.service.TreeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TreeServiceImpl implements TreeService {
     @Autowired
@@ -14,5 +16,30 @@ public class TreeServiceImpl implements TreeService {
     @Override
     public Tree addtree(Tree tree) {
         return treeRepository.save(tree);
+    }
+
+    @Override
+    public List<Tree> getTrees() {
+        return treeRepository.findAll();
+    }
+
+    @Override
+    public Tree updateTree(int id, Tree tree) {
+        Tree treeFind = getTree(id);
+        treeFind.setPlantingDate(tree.getPlantingDate());
+        treeFind.setField(tree.getField());
+        return treeRepository.save(treeFind);
+    }
+
+    @Override
+    public Tree getTree(int id) {
+        return treeRepository.findById(id).orElseThrow(() -> new RuntimeException("Field not found"));
+    }
+
+    @Override
+    public boolean deleteTree(int id) {
+        Tree treeFind = getTree(id);
+        treeRepository.delete(treeFind);
+        return true;
     }
 }
