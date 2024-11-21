3fd29a6ad4c7e3bf2bdad54bd74fadb5fb444c4c
Khadija2504 <khadijalady25@gmail.com>
2024-11-21 21:56:52 +0100

    CIT-23 make the field has the same season in deferent years but can't has the same season in the same years

--- src/main/java/com/Citronix/Citronix/service/impl/HarvestServiceImpl.java
+++ src/main/java/com/Citronix/Citronix/service/impl/HarvestServiceImpl.java
@@ -1,13 +1,10 @@
 package com.Citronix.Citronix.service.impl;
 
-import com.Citronix.Citronix.model.Field;
 import com.Citronix.Citronix.model.Harvest;
 import com.Citronix.Citronix.repository.HarvestRepository;
 import com.Citronix.Citronix.service.HarvestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class HarvestServiceImpl implements HarvestService {
@@ -15,7 +12,8 @@ public class HarvestServiceImpl implements HarvestService {
     private HarvestRepository harvestRepository;
     @Override
     public Harvest addHarvest(Harvest harvest) {
-        boolean exists = harvestRepository.existsByFieldAndSeason(harvest.getField(), harvest.getSeason());
+        int year = harvest.getHarvestDate().getYear();
+        boolean exists = harvestRepository.existsByFieldAndSeasonAndYear(harvest.getField(), harvest.getSeason(), year);
         if (exists) {
             throw new IllegalStateException("a harvest for this field and season already exist");
         }
